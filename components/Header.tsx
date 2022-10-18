4ed49acf7361a37c5b0cfdf9180c7bf1cf48befe
Beny <b.ntambwe97@gmail.com>
2022-10-18 14:23:58 +0200

    Redux Store, Shopping Bag

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import Image from "next/image";
 import Link from "next/link";
 import { SearchIcon, ShoppingBagIcon, UserIcon } from '@heroicons/react/solid';
+import { useSelector } from "react-redux";
+import { selectBasketItems } from "../redux/basketSlice";
 
 function Header() {
 const session = false;
+const items = useSelector(selectBasketItems);
 
     return (
         <header className='sticky top-0 z-30 flex w-full items-end justify-between bg-[#E7ECEE] p-4'>
@@ -29,7 +32,7 @@ const session = false;
                 <Link href="/checkout">
                     <div className='relative cursor-pointer'>
                     <span className='absolute -right-1 -top-1 z-50 flex h-4  w-4 items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 text-[10px] text-white'>
-                        5
+                        {items.length}
                     </span>
                     <ShoppingBagIcon className='headerIcon'/>
                 </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { TableIcon } from '@heroicons/react/solid'
 import type { NextPage, GetServerSideProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Basket from '../components/Basket'
 import Header from '../components/Header'
 import Landing from '../components/Landing'
 import Product from '../components/Product'
@@ -28,7 +29,11 @@ const Home = ({categories, products}: Props) => {
         <title>Apple Redesign</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Header/>
+
+      <Basket/>
+
       <main className='relative h-[200vh] bg-[#E7ECEE]'>
         <Landing/>
       </main>
