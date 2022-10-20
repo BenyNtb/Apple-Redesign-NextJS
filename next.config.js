09574bdbc085de40a831d3bb8abd5dfb8cc5bc9a
Beny <b.ntambwe97@gmail.com>
2022-10-20 23:07:13 +0200

    Extensions

--- next.config.js
+++ next.config.js
@@ -8,5 +8,5 @@ module.exports = {
     locales: ['EN', 'FR'],
     defaultLocale: 'EN',
   },
-  // pageExtensions:['page.tsx', 'page.ts', 'page.jsx', 'page.js']
+  pageExtensions:['page.tsx', 'page.ts', 'page.jsx', 'page.js']
 };
